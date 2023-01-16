fc861dcbff15cc07d0ce547428d4e1a0a520b27a
qWojtpl <71585628+qWojtpl@users.noreply.github.com>
2023-01-16 22:30:11 +0100

    Moving to another directory..

--- plugin/main/java/pl/dailytasks/util/RandomNumber.java
+++
@@ -1,9 +0,0 @@
-package pl.dailytasks.util;
-
-public class RandomNumber {
-
-    public static int randomInt(int min, int max) {
-        return (int) Math.floor(Math.random() * (max - min + 1) + min);
-    }
-
-}
